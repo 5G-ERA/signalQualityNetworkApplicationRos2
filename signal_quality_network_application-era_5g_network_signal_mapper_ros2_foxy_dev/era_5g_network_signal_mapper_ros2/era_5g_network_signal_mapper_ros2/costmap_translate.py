f0ccd08c6f1871ce0b35ddc2860028b1ec5bf3ef
Vladimir <you@example.com>
2023-08-14 11:50:32 +0100

    Cleaned code and refact param and do_trans_cloud..

--- signal_quality_network_application-era_5g_network_signal_mapper_ros2_foxy_dev/era_5g_network_signal_mapper_ros2/era_5g_network_signal_mapper_ros2/costmap_translate.py
+++ signal_quality_network_application-era_5g_network_signal_mapper_ros2_foxy_dev/era_5g_network_signal_mapper_ros2/era_5g_network_signal_mapper_ros2/costmap_translate.py
@@ -1,21 +1,22 @@
+#!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node
 import numpy as np
 from sensor_msgs.msg import PointCloud2
-import sensor_msgs.msg as msg
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 from geometry_msgs.msg import Pose, Point, Quaternion
 import sensor_msgs_py.point_cloud2 as pcl2
 from nav_msgs.srv import GetMap
 from rclpy.clock import Clock
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
+import era_5g_network_signal_mapper_ros2.declare_param as paramm
 
 # List of colours from the PointCloud2 that should not be treated as obstacles by the ROS Navigation Stack.
 ACCEPTED_COLOURS = []
 
-
 def metadata_manual_parser(data: str)->dict:
-    '''
+    
     w_temp = data.find("width")
     coma_index = 0
     for x in range(w_temp,w_temp+10):
@@ -67,8 +68,7 @@ def metadata_manual_parser(data: str)->dict:
     resolution = float(data[int(resolution_temp)+11:int(coma_resolution_temp)])
     print("resolution: "+str(resolution))
 
-	'''
-    metadata = {"width": 1695, "height": 2109, "origin_x": -17.1, "origin_y":-31.6, "resolution": 0.02}
+    metadata = {"width": width, "height": height, "origin_x": origin_x, "origin_y":origin_y, "resolution": resolution}
 
     return metadata
 
@@ -76,15 +76,19 @@ class Colour():
     def __init__(self, rgb):
         self.rgb = rgb
         self.binary = int.from_bytes(bytes(rgb[2:0:-1]), byteorder='little', signed=False)
+
 Green = Colour([124, 252, 0])
 Red = Colour([255, 0, 0])
 Blue = Colour([128, 0, 0])
 Yellow = Colour([255, 255, 0])
 Orange = Colour([255, 165, 0])
+GreenInt = 8190976
 
 ACCEPTED_COLOURS.append(Green.binary)
+ACCEPTED_COLOURS.append(GreenInt)
+
 class Map(object):
-    def __init__(self, origin_x=-50, origin_y=-50, resolution=0.02, width=50, height=50):
+    def __init__(self, origin_x, origin_y, resolution, width, height):
         self.origin_x = origin_x
         self.origin_y = origin_y
         self.resolution = resolution
@@ -92,31 +96,14 @@ class Map(object):
         self.height = height
         self.grid = np.full((height, width), -1.)
 
-    def to_message(self):
-        """ Return a nav_msgs/OccupancyGrid representation of this map. """
-        self.grid_msg = OccupancyGrid()
-        # Set up the header.
-        self.grid_msg.header.stamp = self.get_clock().now().to_msg()
-        self.grid_msg.header.frame_id = 'map'
-        # .info is a nav_msgs/MapMetaData message.
-        self.grid_msg.info.resolution = self.resolution
-        self.grid_msg.info.width = self.width
-        self.grid_msg.info.height = self.height
-        # Rotated maps are not supported... quaternion represents no rotation.
-        self.grid_msg.info.origin = Pose(Point(self.origin_x, self.origin_y, 0), Quaternion(0, 0, 0, 1))
-        # Flatten the numpy array into a list of integers from 0-100.
-        self.flat_grid = self.grid.reshape((self.grid.size,))
-        self.final = list(np.round(self.flat_grid))
-        self.grid_msg.data = self.final
-        return self.grid_msg
-    
-    def toMap(self, node, map_data):
+    def toMap(self, node, map_data,map_frame: str):
+
         """ Return a nav_msgs/OccupancyGrid representation of this map. """
         self.grid_msg = OccupancyGrid()
         # Set up the header.
         time_stamp = Clock().now()
         self.grid_msg.header.stamp = time_stamp.to_msg()
-        self.grid_msg.header.frame_id = 'map'
+        self.grid_msg.header.frame_id = map_frame
         # .info is a nav_msgs/MapMetaData message.
         self.grid_msg.info.resolution = self.resolution
         self.grid_msg.info.width = self.width
@@ -129,54 +116,17 @@ class Map(object):
         point_map = Point(x=float(self.origin_x ), y=float(self.origin_y), z=0.0)
         quat =  Quaternion(x=0.0,y=0.0,z=0.0,w=1.0)
         self.grid_msg.info.origin = Pose(position=point_map, orientation=quat)
-        #node.get_logger().info('map_data.map.data '+str(map_data.map.data[:]))
         self.grid_msg.data = map_data.map.data#[:]
         return self.grid_msg
     
-    '''
-    def toMap(self, map_data):
-            """ Return a nav_msgs/OccupancyGrid representation of this map. """
-            self.grid_msg = OccupancyGrid()
-            # Set up the header.
-            time_stamp = Clock().now()
-            self.grid_msg.header.stamp = time_stamp.to_msg()
-            self.grid_msg.info.origin = Pose(Point(-6.28, -5.16, 0), Quaternion(0, 0, 0, 1))
-            self.grid_msg.header.frame_id = 'map'
-            # .info is a nav_msgs/MapMetaData message.
-            self.grid_msg.info.resolution = 0.05
-            self.grid_msg.info.width = 275
-            self.grid_msg.info.height = 246
-            # Rotated maps are not supported... quaternion represents no rotation.
-            self.grid_msg.data = map_data
-            return self.grid_msg
-    '''
     
 class Mapper(Node):
     def __init__(self):
         super().__init__('pcl2_to_costmap')
-        map_frame = 'map'
-        map_topic = '/map'
-        map_topic_metadata = '/robot/map_metadata'
-        '''ROS node definition'''
 
-        #self.res = None
-        #self.o = None
-        #self.grid_msg = None
-        #self._map = None
-
-        # Wait for the map_metadata and map topics to become available
-        #self.metadata_sub = self.create_subscription(MapMetaData, map_topic_metadata, self.map_metadata_callback, 1)
-        #self.metadata_sub
-
-        self.res = 0.05# data.resolution
-        self.w = 275 #data.width
-        self.h = 246 #data.height
-        #self.o = data.origin
-        # Create map
-        
-        #self.grid_msg = self._map.toMap()
         self.response = None
-        
+        # Retreive parameters from ENV/ launch/params.yaml file or set default values automaticaly
+        self.map_frame: str = paramm.param_set_string(self,'my_map_frame', 'map')       
 
         # CALL SERVICE MAP
         self.get_logger().info("Reading data of original map...", once=True)
@@ -199,8 +149,6 @@ class Mapper(Node):
         if future.result() is not None:
             self.response = future.result()
             # Process the response here
-            
-            #print(str(self.response.map.data[0]))
             metadata = metadata_manual_parser(str(self.response))
             self.get_logger().info('Received map data form map server!')
             # You can access the map and metadata using response.map and response.map_metadata
@@ -208,12 +156,7 @@ class Mapper(Node):
             self.get_logger().info('Failed to receive map data.')
 
         self._map = Map(metadata['origin_x'], metadata['origin_y'], metadata['resolution'], metadata['width'], metadata['height'])
-        #self.map_sub = self.create_subscription(OccupancyGrid, map_topic, self.map_callback, 1)
-        #self.map_sub
-        self.grid_msg = self._map.toMap(self, self.response)
-
-        #self._map_metadata_pub.publish(self.grid_msg.info)
-        #self._map_pub.publish(self.grid_msg)
+        self.grid_msg = self._map.toMap(self, self.response, self.map_frame)
 
         # Create publishers
         self._map_pub = self.create_publisher(
@@ -226,7 +169,6 @@ class Mapper(Node):
         self.get_logger().info("Publishing semantic map.", once=True)
         
     def occupancygrid_to_numpy(self, msg):
-        #self.get_logger().info(str(msg))
         data = np.asarray(msg.data, dtype=np.int8).reshape(msg.info.height, msg.info.width)
         return np.ma.array(data, mask=data==-1, fill_value=-1)
     
@@ -240,13 +182,12 @@ class Mapper(Node):
             arr = arr.data
         grid.data = arr.ravel()
         grid.header.stamp = self.get_clock().now().to_msg()
-        grid.header.frame_id = 'map'
+        grid.header.frame_id = self.map_frame
         grid.info = info
         return grid
     
     def pcl_callback(self, pointcloud_msg):
         
-        #self.get_logger().info(str(pointcloud_msg), once=True)
         pcl_cloud = list(pcl2.read_points(pointcloud_msg, skip_nans=True))
         # To manipulate the OccupancyGrid and add the pcl2 data, first it needs to be translated into a numpy MaskedArray.
         np_occupancy = self.occupancygrid_to_numpy(self.grid_msg)
@@ -254,86 +195,30 @@ class Mapper(Node):
         self.get_logger().info("ACCEPTED_COLOURS "+str(ACCEPTED_COLOURS), once=True)
         try:
             for point in pcl_cloud:
-                #print(point)
                 # Only pcl points with colour not in the accepted array will be considered obstacles and added to the grid with prob(100)
-                #if point[3] not in ACCEPTED_COLOURS:
-                if point[3] !=  8190976: # not in ACCEPTED_COLOURS:
+                if point[3] not in ACCEPTED_COLOURS:
+               
                     self.get_logger().info("point[3] "+str(point[3]), once=True)
-                    #y = int((point[0] - self._map.origin_x) / self._map.resolution)
-                    #x = int((point[1] + self._map.origin_y*1 - 9) / self._map.resolution)
-
                     y = int((point[0] - self._map.origin_x) / self._map.resolution)
                     x = int((point[1] - self._map.origin_y) / self._map.resolution)
-        
-
-                   # y = int((point[0] - self._map.origin_x) / self._map.resolution)
-                   # x = int((point[1]*50 + self._map.origin_y*1 - 40) / self._map.resolution + 11)
-
-                   # y = int((point[0] - self._map.origin_x) / self._map.resolution)
-                   # x = int((point[1] + self._map.origin_y) / self._map.resolution)
-
                     np_occupancy[x][y] = 100.
             
         except Exception as e:
-            print("ERROR: "+str(e))
-        
+            print("ERROR: "+str(e))        
 
         try:
-            #print(self.grid_msg.info)
-            #map_occupancy_grid = self.numpy_to_occupancy_grid(np_occupancy, self.grid_msg.info)
-            #self._map_metadata_pub.publish(self.grid_msg.info)
             self._map_pub.publish(self.grid_msg)
         except Exception as e:
             print("SECOND ERROR: "+str(e))
-
-    
-    '''Only called once'''
-    def map_metadata_callback(self, data):
-        self.res = data.resolution
-        self.w = data.width
-        self.h = data.height
-        self.o = data.origin
-        # Create map
-        self._map = Map(self.o.position.x, self.o.position.y, self.res, self.w, self.h)
-        self.get_logger().info("Reading metadata of original map...", once=True)
         
 
 def main():
-    
+    rclpy.init()
+    node = Mapper()
     try:
-        rclpy.init()
-        
-        m = Mapper()
-        
-        m.get_logger().info('Waiting for map_server service...')
-        client = m.create_client(GetMap, '/map_server/map')
-        while not client.wait_for_service(timeout_sec=1.0):
-            m.get_logger().info('map_server service not available, waiting...')
-        # Create the request message
-        request = GetMap.Request()
-        # Call the service and get the response
-        future = client.call_async(request)
-        rclpy.spin_until_future_complete(m, future)
-        if future.result() is not None:
-            response = future.result()
-            # Process the response here
-            # print(response)
-            m.get_logger().info('Received map data!')
-            # You can access the map and metadata using response.map and response.map_metadata
-        else:
-            m.get_logger().info('Failed to receive map data.')
-        
-        rclpy.spin(m)
-    except rclpy.exceptions.ROSInterruptException:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
         pass
     finally:
-        m.destroy_node()
+        node.destroy_node()
         rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
--- signal_quality_network_application-era_5g_network_signal_mapper_ros2_foxy_dev/era_5g_network_signal_mapper_ros2/era_5g_network_signal_mapper_ros2/signal_mapper.py
+++ signal_quality_network_application-era_5g_network_signal_mapper_ros2_foxy_dev/era_5g_network_signal_mapper_ros2/era_5g_network_signal_mapper_ros2/signal_mapper.py
@@ -1,17 +1,7 @@
-import math
-
-from geometry_msgs.msg import TransformStamped
-
-import numpy as np
-
-import rclpy
-from rclpy.node import Node
-
-from tf2_ros import TransformBroadcaster
-
 #!/usr/bin/env python3
-import numpy as np
+
 import rclpy
+import numpy as np
 import std_msgs.msg as std_msgs
 import struct
 import tf2_ros
@@ -22,32 +12,22 @@ from geometry_msgs.msg import TransformStamped
 from rclpy.node import Node
 from tf2_ros import TransformBroadcaster
 from tf2_ros import TransformException
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
-import time
-from rclpy.duration import Duration
+import era_5g_network_signal_mapper_ros2.declare_param as paramm
 
 
 class FramePublisher(Node):
 
     def __init__(self):
-        super().__init__('turtle_tf2_frame_publisher')
+        super().__init__('robot_tf2_frame_publisher')
 
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
-        # Declare and acquire `turtlename` parameter
-        # Declare parameters
-        self.declare_parameter("base_link", rclpy.Parameter.Type.STRING)
-        self.declare_parameter("semantic_map_frame", rclpy.Parameter.Type.STRING)
-
-        # Retreive parameters from launch/yaml file 
-        self.robot_base_frame = self.get_parameter("base_link").get_parameter_value().string_value
-        self.robot_base_frame_param = self.get_parameter("base_link")._name
-
-        self.semantic_map_frame = self.get_parameter("semantic_map_frame").get_parameter_value().string_value
-
-        #self.tf_broadcaster = TransformBroadcaster(self)
+        # Retreive parameters from ENV/ launch/params.yaml file or set default values automaticaly
+        self.get_logger().info('Retriving params signal_mapper', once=True)
+        self.robot_base_frame = paramm.param_set_string(self,'my_base_link', 'base_link')
+        self.map_frame = paramm.param_set_string(self,'my_map_frame', 'map')
+        self.semantic_map_frame = paramm.param_set_string(self,'my_semantic_map_frame', 'semantic_map')
 
         global r, g, b, height, lenght, lamba
         r = 124 # 124
@@ -59,31 +39,20 @@ class FramePublisher(Node):
         lamba = 0.05
 
         # Initialize the transform broadcaster
-        
-
-        self.current_pcl_pub = self.create_publisher(PointCloud2, '/current_semantic_pcl', 10)
-        
+        self.current_pcl_pub = self.create_publisher(PointCloud2, '/current_semantic_pcl', 10)        
         self.pcl_colour_subscriber = self.create_subscription(String, "/pcl_colour",self.signal_color_callback,1)
-
-        #self.tf_buffer = Buffer()
-        #self.tf_listener = TransformListener(self.tf_buffer, self)
 
         self.tf_broadcaster = TransformBroadcaster(self)
         self.timer = self.create_timer(1.0, self.on_timer)
 
     def on_timer(self):
             try:
-                
-                #trans = self.tf_buffer.lookup_transform('base_link', 'map',rclpy.time.Time())
-                trans = self.tf_buffer.lookup_transform("map", "robot/base_link", rclpy.time.Time()) # trans = self.tf_buffer.lookup_transform("base_link", "map", rclpy.time.Time())
+                trans = self.tf_buffer.lookup_transform(self.map_frame, self.robot_base_frame, rclpy.time.Time())
                 self.publish_pointcloud_while_broadcasting(trans)
                 self.create_simple_pointcloud()
 
             except TransformException as ex:
                     self.get_logger().info(str(ex))
-                    #return
-        
-
     
     def publish_pointcloud_while_broadcasting(self,trans):
             
@@ -92,15 +61,10 @@ class FramePublisher(Node):
             # Read message content and assign it to
             # corresponding tf variables
             t.header.stamp = self.get_clock().now().to_msg()
-            t.header.frame_id = 'map'
+            t.header.frame_id = self.map_frame
             t.child_frame_id = self.semantic_map_frame
             # Robot only exists in 2D, thus we get x and y translation
             # coordinates from the message and set the z coordinate to 0
-            roll, pitch, yaw = self.euler_from_quaternion(
-                trans.transform.rotation.x,
-                trans.transform.rotation.y,
-                trans.transform.rotation.z,
-                trans.transform.rotation.w) 
             t.transform.translation.x =   trans.transform.translation.x
             t.transform.translation.y = trans.transform.translation.y
             t.transform.translation.z = trans.transform.translation.z
@@ -108,7 +72,6 @@ class FramePublisher(Node):
             # For the same reason, robot can only rotate around one axis
             # and this why we set rotation in x and y to 0 and obtain
             # rotation in z axis from the message
-            #q = tf_transformations.quaternion_from_euler(0, 0, msg.theta)
             t.transform.rotation.x = trans.transform.rotation.x
             t.transform.rotation.y = trans.transform.rotation.y
             t.transform.rotation.z = trans.transform.rotation.z
@@ -117,38 +80,10 @@ class FramePublisher(Node):
             # Send the transformation
             self.tf_broadcaster.sendTransform(t)
 
-    def euler_from_quaternion(self, x, y, z, w):
-        '''
-        Convert a quaternion into euler angles (roll, pitch, yaw)
-        roll is rotation around x in radians (counterclockwise)
-        pitch is rotation around y in radians (counterclockwise)
-        yaw is rotation around z in radians (counterclockwise)
-        '''
-
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
-        
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
-        
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
-        
-        return roll_x, pitch_y, yaw_z # in radians
-
-
     # Set pcl colour callback
     def signal_color_callback(self, msg):
-        self.get_logger().info("COLOR CHANGED!!")
-        self.get_logger().info(msg.data)
-        '''
-        Yellow = Colour([255, 255, 0])
-        Orange = Colour([255, 165, 0])
-        '''
+        self.get_logger().info(str(msg.data) + " COLOR REQUESTED TO CHANGE!!!")
+
         global r, g, b
         if msg.data == "GREEN":
             self.get_logger().info("CHANGE TO GREEN")
@@ -162,21 +97,19 @@ class FramePublisher(Node):
             b = 0
         elif msg.data == "BLUE":
             self.get_logger().info("CHANGE TO BLUE")
-            r = 128
+            r = 0
             g = 0
-            b = 0
+            b = 255
         elif msg.data == "YELLOW":
-            self.get_logger().info("CHANGE TO BLUE")
+            self.get_logger().info("CHANGE TO YELLOW")
             r = 255
             g = 255
             b = 0
         elif msg.data == "ORANGE":
-            self.get_logger().info("CHANGE TO BLUE")
+            self.get_logger().info("CHANGE TO ORANGE")
             r = 255
             g = 165
             b = 0
-
-    
 
     def create_simple_pointcloud(self):
 
@@ -218,6 +151,6 @@ def main():
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-
-    rclpy.shutdown()
-
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
