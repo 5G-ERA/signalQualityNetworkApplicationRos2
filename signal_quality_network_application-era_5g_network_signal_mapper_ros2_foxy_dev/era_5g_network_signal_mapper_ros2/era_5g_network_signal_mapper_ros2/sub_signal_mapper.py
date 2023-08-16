28d73f64fdcfc58bb3baf5b041d5bb9ea6a19b82
Vladimir <you@example.com>
2023-08-16 09:37:24 +0100

    Doc: added detailed comments.

--- signal_quality_network_application-era_5g_network_signal_mapper_ros2_foxy_dev/era_5g_network_signal_mapper_ros2/era_5g_network_signal_mapper_ros2/sub_signal_mapper.py
+++ signal_quality_network_application-era_5g_network_signal_mapper_ros2_foxy_dev/era_5g_network_signal_mapper_ros2/era_5g_network_signal_mapper_ros2/sub_signal_mapper.py
@@ -6,7 +6,6 @@ from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2, PointField
 import std_msgs.msg as std_msgs
 import sensor_msgs_py.point_cloud2 as pcl2
-import time
 from rclpy.duration import Duration
 import era_5g_network_signal_mapper_ros2.tf2_custom as tf2_c
 import era_5g_network_signal_mapper_ros2.declare_param as paramm
@@ -16,8 +15,11 @@ class SubSignalMapper(Node):
     
         def __init__(self):
             super().__init__("pcl2_semantic_mapper")
-
+            # Declare a tf_buffer which will store known frames and offers a ROS service, "tf_frames",
+            # which responds to client requests with a response containing a tf2_msgs
             self.tf_buffer = tf2_ros.Buffer() 
+            # Here, we create a tf2_ros.TransformListener object. Once the listener is created,
+            # it starts receiving tf2 transformations over the wire, and buffers them for up to "timeout" declared "sub_callback(pcl)".
             self.listener = tf2_ros.TransformListener(self.tf_buffer,self)
 
             # Retreive parameters from ENV/ launch/params.yaml file or set default values automaticaly
@@ -30,7 +32,8 @@ class SubSignalMapper(Node):
             # Historical pcl2
             merged_cloud = []
             
-            # Function to construct a pointcloud2 object
+            # Function to construct a pointcloud2 object 
+            # which will represent the collections of current clouds 
             def construct_pointCloud2(points):
                 
                 # ROS DATATYPE 
@@ -52,12 +55,16 @@ class SubSignalMapper(Node):
                 pcl_msg.header.stamp = self.get_clock().now().to_msg()
                 pcl_msg.header.frame_id = self.map_frame
                 return pcl_msg
-
-            def sub_callback(pcl):   
-                   
+            
+            # Creating sub_callback which will receive current cloud of the robot "pcl"
+            def sub_callback(pcl):
+                
+                # the amount of time to wait until fail receiving both frames
                 timeout = Duration(seconds=10)
+                # Waiting until transfrom of map_frame and semantic_map_frame is available
                 transform = self.tf_buffer.lookup_transform('map', 'semantic_map', rclpy.time.Time(), timeout)
-
+                
+                # Atempt to transform frame "map" to "semantic_map" and add cloud received from "pcl"
                 try:
                     transformed_cloud = tf2_c.do_transform_cloud(pcl, transform)
 
@@ -82,8 +89,14 @@ class SubSignalMapper(Node):
                 for y in temp_list:
                     merged_cloud.append(y)
                 # Recreate the merged pointcloud
-                map_pcl = construct_pointCloud2(merged_cloud) 
+                map_pcl = construct_pointCloud2(merged_cloud)
+
+                # Publishing a map created from collection of current semantic cloud of the robot
+                # Rate of pubication is rate of "semantic_pcl_sub" minus time of waiting 
+                # frames for transformation from "self.tf_buffer.lookup_transform...."
                 self.semantic_pcl_pub.publish(map_pcl)
+
+            # Subscribing to "/current_semantic_pcl" for receiving current semantic cloud of the robot
             self.semantic_pcl_sub = self.create_subscription(PointCloud2, "/current_semantic_pcl",sub_callback, qos_profile=1,)
 
 def main():
