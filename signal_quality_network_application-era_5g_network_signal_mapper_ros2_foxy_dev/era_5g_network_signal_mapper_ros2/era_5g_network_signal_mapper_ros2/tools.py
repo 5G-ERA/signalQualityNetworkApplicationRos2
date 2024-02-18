9d5a293d0eb5afd24291551cd36b56d19750954a
appmdev <80983605+appmdev@users.noreply.github.com>
2024-02-18 19:22:21 +0000

    feat: load and save pointcloud data as file "pointcloud_data.txt"
    feat: load and save pointcloud data as file "pointcloud_data.txt"

--- signal_quality_network_application-era_5g_network_signal_mapper_ros2_foxy_dev/era_5g_network_signal_mapper_ros2/era_5g_network_signal_mapper_ros2/tools.py
+++ signal_quality_network_application-era_5g_network_signal_mapper_ros2_foxy_dev/era_5g_network_signal_mapper_ros2/era_5g_network_signal_mapper_ros2/tools.py
@@ -1,9 +1,10 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import PointCloud2
-from std_srvs.srv import Trigger, TriggerResponse
-import sensor_msgs.point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2, PointField
+from std_srvs.srv import *
+import sensor_msgs_py.point_cloud2 as pcl2
 import numpy as np
+import std_msgs.msg as std_msgs
 
 class PointCloudSaverNode(Node):
     def __init__(self):
@@ -23,7 +24,7 @@ class PointCloudSaverNode(Node):
     def save_pointcloud_callback(self, request, response):
         if self.pointcloud_data is not None:
             # Extract point cloud data
-            points = pc2.read_points(self.pointcloud_data, field_names=("x", "y", "z", "rgb"), skip_nans=True)
+            points = pcl2.read_points(self.pointcloud_data, field_names=("x", "y", "z", "rgb"), skip_nans=True)
 
             # Convert points to a list for easy manipulation
             point_list = list(points)
@@ -63,12 +64,37 @@ class PointCloudSaverNode(Node):
             # Write each point to the file
             for point in point_list:
                 x, y, z, rgb = point
-                r = (rgb >> 16) & 0xff
-                g = (rgb >> 8) & 0xff
-                b = rgb & 0xff
-                file.write(f"{x} {y} {z} {r} {g} {b}\n")
+                file.write(f"{x} {y} {z} {rgb}".rstrip()+'\n')
 
     def load_pointcloud_from_file(self, filename):
+        try:
+            # Read PointCloud2 data from the file
+            loaded_points = np.loadtxt(filename, comments="#", delimiter=" ", dtype=float)
+            if len(loaded_points) == 0:
+                self.get_logger().warning("Loaded PointCloud2 data is empty.")
+                return None
+        except Exception as e:
+            self.get_logger().error(f"Error loading PointCloud2 from file: {e}")
+            return None
+         
+        header = std_msgs.Header()
+        header.stamp = self.get_clock().now().to_msg()
+        header.frame_id = "map"
+        # Create a PointCloud2 message and fill it with the loaded data
+        #header = self.pointcloud_data.header if self.pointcloud_data else None
+                # ROS DATATYPE 
+        ros_dtype = PointField.FLOAT32
+        fields = [
+            PointField(name='x', offset=0, datatype=ros_dtype, count=1),
+            PointField(name='y', offset=4, datatype=ros_dtype, count=1),
+            PointField(name='z', offset=8, datatype=ros_dtype, count=1),
+            # The 4th set of bytes represent colour of pointcloud
+            PointField(name='rgb', offset=12, datatype=PointField.FLOAT32, count=1)
+        ]
+        loaded_pointcloud = pcl2.create_cloud(header, fields, loaded_points)
+        return loaded_pointcloud
+
+    def load_pointcloud_from_file2(self, filename):
         try:
             # Read PointCloud2 data from the file
             loaded_points = np.loadtxt(filename, comments="#", delimiter=" ", dtype=float)
@@ -78,9 +104,10 @@ class PointCloudSaverNode(Node):
 
         # Create a PointCloud2 message and fill it with the loaded data
         header = self.pointcloud_data.header if self.pointcloud_data else None
-        loaded_pointcloud = pc2.create_cloud_xyz32(header, loaded_points[:, :3])
+        loaded_pointcloud = pcl2.create_cloud_xyz32(header, loaded_points[:, :3])
 
         return loaded_pointcloud
+    
 
 def main(args=None):
     rclpy.init(args=args)
